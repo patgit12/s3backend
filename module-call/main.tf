ee88a8211dc1316af89001cce7b98df48e69609a
patongit <90884766+patgit12@users.noreply.github.com>
2023-09-25 12:52:41 -0400

    updating

--- module-call/main.tf
+++ module-call/main.tf
@@ -1,4 +1,4 @@
 module "s3backend_mod" {
-  source = "git@github.com:patgit12/s3backend.git//s3_module?ref=v1.2.1" # make sure not to pass a tag as ref instead of release as ref
+  source = "git::https://github.com/patgit12/s3backend.git//s3_module?ref=v1.2.1" # make sure not to pass a tag as ref instead of release as ref
   
 }
